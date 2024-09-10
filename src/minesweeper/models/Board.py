e7a9e5331b60ad4799e2fdb3170540ede8b5819f
Vincent-Jung <117348000+Vincent-Jung@users.noreply.github.com>
2024-09-10 11:44:26 +0200

    feat/difficulty-set-board-size
    Added a match case to get the game difficulty and size the board accordingly.
    Removed size from Board parameters.

--- src/minesweeper/models/Board.py
+++ src/minesweeper/models/Board.py
@@ -2,27 +2,36 @@ import random
 from pprint import pprint
 
 class Board:
-    def __init__(self, difficulty: str, size: int, mines: int):
+    def __init__(self, difficulty: str, mines: int):
         self.difficulty = difficulty
-        self.size = size
         self.mines = mines
+        self._generate_board(self.difficulty)
         
-            # difficulty
+        # difficulty
         # EASY = 9x9, 10 mines
         # NORMAL = 16x16, 40 mines
         # HARD = 30x16, 99 mines
-
-    def generate_board(self):
-        board = [[0 for _ in range(9)] for _ in range(9)]  # 1st range = col, 2d range = rows. Hard=30, 16
-        pprint(board)
     
+    def generate_board (self, col , row):
+        return [[0 for _ in range(col)] for _ in range(row)]
+
+    def _generate_board(self, game_difficulty):
+        board = None
+        match game_difficulty:
+            case "easy":
+                board = self.generate_board(9 , 9)  # 1st range = col, 2d range = rows
+            case "normal":
+                board = self.generate_board(16 , 16) 
+            case "hard":
+                board = self.generate_board(20 , 16) 
+                
+        pprint(board)
+        return board
+
     def random_mines(self):
         pass
     
     def map_numbers(self):
         pass
     
-
-
-my_board = Board("easy", 9, 10)
-my_board.generate_board()
+my_board = Board("easy", 10)
