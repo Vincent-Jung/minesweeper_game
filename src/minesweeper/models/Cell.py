ff215d6c1aaffbf6efa8eadf49d563b7fc705a36
Vincent-Jung <117348000+Vincent-Jung@users.noreply.github.com>
2024-09-11 11:51:03 +0200

    logic/cells-from-Cell
    All Board logic now call Cell() class to generate a board made of cells.
    Changed back the match case to elif in order to save lines without Dict.
    Added display_board function for visualizing the board in terminal.
    Added comments ont functions.

--- src/minesweeper/models/Cell.py
+++ src/minesweeper/models/Cell.py
@@ -1,5 +1,5 @@
 class Cell:
-    def __init__(self, is_revealed: bool, is_flagged: bool, is_a_mine: bool, adjacent_mines: int):
+    def __init__(self, is_revealed: bool = False, is_flagged: bool = False, is_a_mine: bool = False, adjacent_mines: int = ""):
         self.is_revealed = is_revealed
         self.is_flagged = is_flagged
         self.is_a_mine = is_a_mine
@@ -9,12 +9,17 @@ class Cell:
         return f"Revealed: {self.is_revealed}, Flagged: {self.is_flagged}, Mine: {self.is_a_mine}, Adj mines: {self.adjacent_mines}"
     
     def reveal_cell(self):
+        """Reveal a cell on the board
+        """
         self.is_revealed = True
         print(self)
         if self.is_a_mine == True:
             print("** BOOM Game over **")
     
     def toggle_flag(self):
+        """Toggle a non revealed cell with a flag
+        Add a flag on non flagged cell, remove a flag on a flagged cell.
+        """
         if self.is_flagged == False:
             self.is_flagged = True
         else:
@@ -23,7 +28,8 @@ class Cell:
     def calculate_number(self):
         pass
         
-        
+
+        # Tests
 cA1 = Cell(False, False, False, 1)  # no Mine
 cA2 = Cell(False, False, True, 1)  # Mine !
 cA3 = Cell(False, True, False, 1)
